a140546c5de55d10868a00f5a177af2852a8824a
Shreyas <shreyas@everwell.org>
2020-05-12 22:47:38 +0530

    added toss winner and battingfirst

--- getPayloadForMatch.js
+++ getPayloadForMatch.js
@@ -4,13 +4,12 @@ const puppeteer = require('puppeteer');
 const fs = require('fs');
 
 function getDetails() {
-    let details = {
-
-    };
+    let details = {};
 
     try {
+        let team1;
+        let team2;
         let teamsElements = document.querySelectorAll('.cb-minfo-tm-nm');
-        debugger;
         if (teamsElements.length === 6) {
             for (let index in teamsElements) {
                 index = parseInt(index, 10);
@@ -18,37 +17,47 @@ function getDetails() {
 
                 if (index === 0) {
                     let text = teamElement.innerText;
-                    let team1 = text.replace('Squad', '').trim();
+                    team1 = text.replace('Squad', '').trim();
                     details.team1 = team1;
                 }
 
                 if (index === 3) {
                     let text = teamElement.innerText;
-                    let team2 = text.replace('Squad', '').trim();
+                    team2 = text.replace('Squad', '').trim();
                     details.team2 = team2;
                 }
             }
         }
+
+        let matchInfoElements = document.querySelectorAll('.cb-mtch-info-itm');
+        for (let matchInfoElement of matchInfoElements) {
+            let divs = matchInfoElement.querySelectorAll('div');
+            let fieldName = divs[0].innerText;
+            let fieldValue = divs[1].innerText;
+
+            if (fieldName === 'Toss') {
+                if (fieldValue.indexOf(' won the toss ') !== -1) {
+                    let matches = fieldValue.match(/(.*) won the toss and opt to (.*)/);
+                    let tossWinner = matches[1];
+                    let decision = matches[2];
+                    details.tossWinner = tossWinner;
+                    let batFirst = tossWinner;
+                    if (decision === 'bowl') {
+                        if (team1 === tossWinner) {
+                            batFirst = team2;
+                        } else {
+                            batFirst = team1;
+                        }
+                    }
+                    details.batFirst = batFirst;
+
+                }
+            }
+        }
+
     } catch(e) {
         console.log(e);
     }
-
-
-
-    // let matchInfoElements = document.querySelectorAll('.cb-mtch-info-itm');
-    // for (let matchInfoElement of matchInfoElements) {
-    //     let divs = matchInfoElement.querySelectorAll('div');
-    //     let fieldName = divs[0].innerText;
-    //     let fieldValue = divs[1].innerText;
-    //     // console.log(fieldName);
-    //     // details[fieldName] = fieldValue;
-
-    //     if (fieldName === 'Toss') {
-    //         if (fieldValue.indexOf(' won the toss ') !== -1) {
-    //             let matches =
-    //         }
-    //     }
-    // }
 
     return details;
 }
@@ -75,6 +84,7 @@ function getDetails() {
     // const scores = await page.evaluate(getScores);
 
     // fs.writeFile('scores.json', JSON.stringify(scores, null, ' '));
+    await page.close();
 
     await browser.close();
 })();
