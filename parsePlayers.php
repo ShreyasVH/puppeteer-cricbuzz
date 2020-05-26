504adcb83bbe2948f15b7fcd2e925b976e8336f0
Shreyas <shreyas@everwell.org>
2020-05-26 21:05:06 +0530

    added script for parsing players from data

---
+++ parsePlayers.php
@@ -0,0 +1,58 @@
+<?php
+
+define('APP_PATH', realpath(dirname(__FILE__)) . '/');
+
+$dataDirectory = APP_PATH . 'data/yearWiseDetails';
+
+$files = scandir($dataDirectory);
+$files = array_filter($files, function($file){
+	return (($file !== '.') && ($file !== '..'));
+});
+
+$playerData = [];
+
+foreach($files as $file)
+{
+	$data = json_decode(file_get_contents($dataDirectory . '/' . $file), true);
+
+	foreach($data as $seriesName => $seriesDetails)
+	{
+		echo "\n" . $seriesName . "\n";
+		foreach($seriesDetails as $matchName => $matchDetails)
+		{
+			echo "\n\t" . $matchName . "\n";
+			if(array_key_exists('players', $matchDetails))
+			{
+				$players = $matchDetails['players'];
+
+				foreach($players as $playerDetails)
+				{
+					$team = $playerDetails['team'];
+					$playerName = $playerDetails['player'];
+					echo "\n\t\t" . $team . "\n";
+					echo "\n\t\t" . $playerName . "\n";
+					if(array_key_exists($team, $playerData))
+					{
+						$existingPlayers = $playerData[$team];
+						// var_dump($existingPlayers);die;
+						if(!in_array($playerName, $existingPlayers))
+						{
+							$playerData[$team][] = $playerName;
+						}
+						 
+					}
+					else
+					{
+						$playerData[$team] = [
+							$playerName
+						];
+						// var_dump($playerDetails);die;
+					}
+				}
+			}
+			
+		}
+	}
+}
+
+file_put_contents(APP_PATH . 'data/players.json', json_encode($playerData, JSON_PRETTY_PRINT));
