e223c20b0f83be2b07bae36a538c998131243f43
Shreyas <shreyas.hande@gmail.com>
2020-12-23 13:39:02 +0530

    updated retired hurt regex

--- src/getMatchDetails.js
+++ src/getMatchDetails.js
@@ -162,7 +162,7 @@ const getMatchDetailsFromHTML = (teamReplacements, getPlayerIdFromLinkDef, getGa
                             const hitWicketRegex = /^(hit wicket|hit wkt) b (.*)$/;
                             const obstructedRegex = /^obs|obstructing the field$/;
                             const timedOutRegex = /timed out/;
-                            const retiredHurtRegex = /retd hurt|retired hurt|retired ill/;
+                            const retiredHurtRegex = /retd hurt|retired hurt|retired ill|retired out/;
                             const handledRegex = /handled the ball/;
 
                             if (dismissalModeText.match(/not out/)) {
