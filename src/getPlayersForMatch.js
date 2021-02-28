603bf50274ded008a695c9cb410eb9bba5fffe22
Shreyas <shreyas.hande@gmail.com>
2021-02-28 17:41:29 +0530

    exporting as a function

--- src/getPlayersForMatch.js
+++ src/getPlayersForMatch.js
@@ -4,22 +4,36 @@ const fs = require('fs');
 const path = require('path');
 const getPlayerIdFromLink = require('./utils').getPlayerIdFromLink;
 
+const scriptName = path.basename(__filename);
 
-let playerIds = [];
+const fileNameParts = process.argv[1].split('\/');
+const fileName = fileNameParts[fileNameParts.length - 1];
 
-const tourId = process.argv[2];
-const matchId = process.argv[3];
+const getPlayersForMatch = (tourId, matchId) => {
+    let playerIds = [];
 
-const tourFolder = path.resolve(__dirname, '../data/matches/' + tourId);
-const matchFilePath = tourFolder + '/' + matchId + '.json';
+    const tourFolder = path.resolve(__dirname, '../data/matches/' + tourId);
+    const matchFilePath = tourFolder + '/' + matchId + '.json';
 
-if (fs.existsSync(tourFolder) && fs.existsSync(matchFilePath)) {
-    const details = JSON.parse(fs.readFileSync(matchFilePath));
-    if (details.hasOwnProperty('players')) {
-        for (const player of details.players) {
-            playerIds.push(getPlayerIdFromLink(player.link));
+    if (fs.existsSync(tourFolder) && fs.existsSync(matchFilePath)) {
+        const details = JSON.parse(fs.readFileSync(matchFilePath));
+        if (details.hasOwnProperty('players')) {
+            for (const player of details.players) {
+                playerIds.push(getPlayerIdFromLink(player.link));
+            }
         }
     }
-}
 
-fs.writeFileSync(path.resolve(__dirname, '../data/playerIdsForMatch.csv'), playerIds.join("\n"));
+    return playerIds;
+}
+exports.getPlayersForMatch = getPlayersForMatch;
+
+if (fileName === scriptName) {
+    (async() => {
+        const tourId = process.argv[2];
+        const matchId = process.argv[3];
+
+        const playerIds = getPlayersForMatch(tourId, matchId);
+        fs.writeFileSync(path.resolve(__dirname, '../data/playerIdsForMatch.csv'), playerIds.join("\n"));
+    })();
+}
