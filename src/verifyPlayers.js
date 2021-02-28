f55134398d35ee6f567e343d6d1d80fb2f2fcb86
Shreyas <shreyas.hande@gmail.com>
2021-02-28 17:42:52 +0530

    splitting data into 2 files

--- src/verifyPlayers.js
+++ src/verifyPlayers.js
@@ -62,7 +62,19 @@ const getCreatedPlayerDetails = async id => {
         IPL: 'IPL'
     };
 
+    const mainLineCountries = [
+        'India',
+        'Australia',
+        'South Africa',
+        'New Zealand',
+        'England',
+        'Pakistan',
+        'Sri Lanka',
+        'West Indies'
+    ];
+
     let mismatchedPlayerStats = {};
+    let mismatchedPlayerStatsAssociates = {};
 
     const now = (new Date()).getTime();
     let index = 0;
@@ -76,7 +88,7 @@ const getCreatedPlayerDetails = async id => {
 
         let playerCache = JSON.parse(fs.readFileSync(path.resolve(__dirname, '../data/playerCache.json')));
         let playerDetails;
-        if (playerCache.hasOwnProperty(playerId) && playerCache[playerId].updated && (playerCache[playerId].updated >= (now - (24 * 3600 * 1000)))) {
+        if (playerCache.hasOwnProperty(playerId) && playerCache[playerId].updated && (playerCache[playerId].updated >= (now - (3 * 24 * 3600 * 1000)))) {
             playerDetails = playerCache[playerId];
         } else {
             playerDetails = await getPlayerDetails(playerId);
@@ -89,181 +101,195 @@ const getCreatedPlayerDetails = async id => {
             const createdPlayerDetailsResponse = await getCreatedPlayerDetails(createdPlayerId);
             const createdPlayerDetails = createdPlayerDetailsResponse.result;
 
-            const expectedBattingStats = playerDetails.battingStats;
-            const actualBattingStats = createdPlayerDetails.battingStats;
-            for (const [gameTypeRaw, stats] of Object.entries(expectedBattingStats)) {
-                if (stats.innings == '-') {
-                    continue;
-                }
+            const country = createdPlayerDetails.country.name;
+            const isAssociateCountry = (mainLineCountries.indexOf(country) === -1);
 
-                const gameType = formattedGameTypes[gameTypeRaw];
-                if (gameType === 'IPL') {
-                    continue;
-                }
+            if (playerDetails.battingStats) {
+                const expectedBattingStats = playerDetails.battingStats;
+                const actualBattingStats = createdPlayerDetails.battingStats;
+                for (const [gameTypeRaw, stats] of Object.entries(expectedBattingStats)) {
+                    if (stats.innings == '-') {
+                        continue;
+                    }
 
-                let gameTypeStats = {};
+                    const gameType = formattedGameTypes[gameTypeRaw];
+                    if (gameType === 'IPL') {
+                        continue;
+                    }
 
-                if (actualBattingStats.hasOwnProperty(gameType)) {
-                    const actualStats = actualBattingStats[gameType];
+                    let gameTypeStats = {};
 
-                    const expectedRuns = stats.runs;
-                    const actualRuns = actualStats.runs;
-                    if (expectedRuns != actualRuns) {
-                        gameTypeStats.runs = {
-                            expectedRuns,
-                            actualRuns,
+                    if (actualBattingStats.hasOwnProperty(gameType)) {
+                        const actualStats = actualBattingStats[gameType];
+
+                        const expectedRuns = stats.runs;
+                        const actualRuns = actualStats.runs;
+                        if (expectedRuns != actualRuns) {
+                            gameTypeStats.runs = {
+                                expectedRuns,
+                                actualRuns,
+                                gameType
+                            };
+                        }
+
+                        const expectedInnings = stats.innings;
+                        const actualInnings = actualStats.innings;
+                        if (expectedInnings != actualInnings) {
+                            gameTypeStats.innings = {
+                                expectedInnings,
+                                actualInnings,
+                                gameType
+                            };
+                        }
+
+                        // const expectedNotOuts = stats.notOuts;
+                        // const actualNotOuts = actualStats.notOuts;
+                        // if (expectedNotOuts != actualNotOuts) {
+                        //     gameTypeStats.notOuts = {
+                        //         expectedNotOuts,
+                        //         actualNotOuts,
+                        //         gameType
+                        //     };
+                        // }
+
+                        const expectedBalls = stats.balls;
+                        const actualBalls = actualStats.balls;
+                        if (expectedBalls != actualBalls) {
+                            gameTypeStats.balls = {
+                                expectedBalls,
+                                actualBalls,
+                                gameType
+                            };
+                        }
+
+                        const expectedFours = stats.fours;
+                        const actualFours = actualStats.fours;
+                        if (expectedFours != actualFours) {
+                            gameTypeStats.fours = {
+                                expectedFours,
+                                actualFours,
+                                gameType
+                            };
+                        }
+
+                        const expectedSixes = stats.sixes;
+                        const actualSixes = actualStats.sixes;
+                        if (expectedSixes != actualSixes) {
+                            gameTypeStats.sixes = {
+                                expectedSixes,
+                                actualSixes,
+                                gameType
+                            };
+                        }
+
+                        const expectedHighest = stats.highest;
+                        const actualHighest = actualStats.highest;
+                        if (expectedHighest != actualHighest) {
+                            gameTypeStats.highest = {
+                                expectedHighest,
+                                actualHighest,
+                                gameType
+                            };
+                        }
+                    } else {
+                        gameTypeStats.missingGameType = {
                             gameType
                         };
                     }
 
-                    const expectedInnings = stats.innings;
-                    const actualInnings = actualStats.innings;
-                    if (expectedInnings != actualInnings) {
-                        gameTypeStats.innings = {
-                            expectedInnings,
-                            actualInnings,
-                            gameType
-                        };
+                    if (Object.keys(gameTypeStats).length > 0) {
+                        mismatchedStats.push(gameTypeStats);
                     }
-
-                    const expectedNotOuts = stats.notOuts;
-                    const actualNotOuts = actualStats.notOuts;
-                    if (expectedNotOuts != actualNotOuts) {
-                        gameTypeStats.notOuts = {
-                            expectedNotOuts,
-                            actualNotOuts,
-                            gameType
-                        };
-                    }
-
-                    const expectedBalls = stats.balls;
-                    const actualBalls = actualStats.balls;
-                    if (expectedBalls != actualBalls) {
-                        gameTypeStats.balls = {
-                            expectedBalls,
-                            actualBalls,
-                            gameType
-                        };
-                    }
-
-                    const expectedFours = stats.fours;
-                    const actualFours = actualStats.fours;
-                    if (expectedFours != actualFours) {
-                        gameTypeStats.fours = {
-                            expectedFours,
-                            actualFours,
-                            gameType
-                        };
-                    }
-
-                    const expectedSixes = stats.sixes;
-                    const actualSixes = actualStats.sixes;
-                    if (expectedSixes != actualSixes) {
-                        gameTypeStats.sixes = {
-                            expectedSixes,
-                            actualSixes,
-                            gameType
-                        };
-                    }
-
-                    const expectedHighest = stats.highest;
-                    const actualHighest = actualStats.highest;
-                    if (expectedHighest != actualHighest) {
-                        gameTypeStats.highest = {
-                            expectedHighest,
-                            actualHighest,
-                            gameType
-                        };
-                    }
-                } else {
-                    gameTypeStats.missingGameType = {
-                        gameType
-                    };
-                }
-
-                if (Object.keys(gameTypeStats).length > 0) {
-                    mismatchedStats.push(gameTypeStats);
-                }
-            }
-
-            const expectedBowlingStats = playerDetails.bowlingStats;
-            const actualBowlingStats = createdPlayerDetails.bowlingStats;
-            for (const [gameTypeRaw, stats] of Object.entries(expectedBowlingStats)) {
-                const gameType = formattedGameTypes[gameTypeRaw];
-                if (stats.innings == '-') {
-                    continue;
-                }
-
-                if (gameType === 'IPL') {
-                    continue;
-                }
-
-
-
-                let gameTypeStats = {};
-
-                if (actualBowlingStats.hasOwnProperty(gameType)) {
-                    const actualStats = actualBowlingStats[gameType];
-
-                    const expectedRuns = stats.runs;
-                    const actualRuns = actualStats.runs;
-                    if (expectedRuns != actualRuns) {
-                        gameTypeStats.runsBowl = {
-                            expectedRuns,
-                            actualRuns,
-                            gameType
-                        };
-                    }
-
-                    const expectedInnings = stats.innings;
-                    const actualInnings = actualStats.innings;
-                    if (expectedInnings != actualInnings) {
-                        gameTypeStats.inningsBowl = {
-                            expectedInnings,
-                            actualInnings,
-                            gameType
-                        };
-                    }
-
-                    const expectedWickets = stats.wickets;
-                    const actualWickets = actualStats.wickets;
-                    if (expectedWickets != actualWickets) {
-                        gameTypeStats.wickets = {
-                            expectedWickets,
-                            actualWickets,
-                            gameType
-                        };
-                    }
-
-                    const expectedBalls = stats.balls;
-                    const actualBalls = actualStats.balls;
-                    if (expectedBalls != actualBalls) {
-                        gameTypeStats.ballsBowl = {
-                            expectedBalls,
-                            actualBalls,
-                            gameType
-                        };
-                    }
-                }  else {
-                    gameTypeStats.missingGameType = {
-                        gameType
-                    };
-                }
-
-                if (Object.keys(gameTypeStats).length > 0) {
-                    mismatchedStats.push(gameTypeStats);
                 }
             }
 
+            if (playerDetails.bowlingStats) {
+                const expectedBowlingStats = playerDetails.bowlingStats;
+                const actualBowlingStats = createdPlayerDetails.bowlingStats;
+                for (const [gameTypeRaw, stats] of Object.entries(expectedBowlingStats)) {
+                    const gameType = formattedGameTypes[gameTypeRaw];
+                    if (stats.innings == '-') {
+                        continue;
+                    }
 
+                    if (gameType === 'IPL') {
+                        continue;
+                    }
+
+
+
+                    let gameTypeStats = {};
+
+                    if (actualBowlingStats.hasOwnProperty(gameType)) {
+                        const actualStats = actualBowlingStats[gameType];
+
+                        const expectedRuns = stats.runs;
+                        const actualRuns = actualStats.runs;
+                        if (expectedRuns != actualRuns) {
+                            gameTypeStats.runsBowl = {
+                                expectedRuns,
+                                actualRuns,
+                                gameType
+                            };
+                        }
+
+                        const expectedInnings = stats.innings;
+                        const actualInnings = actualStats.innings;
+                        if (expectedInnings != actualInnings) {
+                            gameTypeStats.inningsBowl = {
+                                expectedInnings,
+                                actualInnings,
+                                gameType
+                            };
+                        }
+
+                        const expectedWickets = stats.wickets;
+                        const actualWickets = actualStats.wickets;
+                        if (expectedWickets != actualWickets) {
+                            gameTypeStats.wickets = {
+                                expectedWickets,
+                                actualWickets,
+                                gameType
+                            };
+                        }
+
+                        // const expectedBalls = stats.balls;
+                        // const actualBalls = actualStats.balls;
+                        // if (expectedBalls != actualBalls) {
+                        //     gameTypeStats.ballsBowl = {
+                        //         expectedBalls,
+                        //         actualBalls,
+                        //         gameType
+                        //     };
+                        // }
+                    }  else {
+                        gameTypeStats.missingGameType = {
+                            gameType
+                        };
+                    }
+
+                    if (Object.keys(gameTypeStats).length > 0) {
+                        mismatchedStats.push(gameTypeStats);
+                    }
+                }
+            }
 
             if (mismatchedStats.length > 0) {
-                mismatchedPlayerStats[playerId] = {
-                    name: playerDetails.name,
-                    createdPlayerId,
-                    country: createdPlayerDetails.country.name,
-                    mismatchedStats
-                };
+                if (isAssociateCountry) {
+                    mismatchedPlayerStatsAssociates[playerId] = {
+                        name: playerDetails.name,
+                        createdPlayerId,
+                        country,
+                        mismatchedStats
+                    };
+                } else {
+                    mismatchedPlayerStats[playerId] = {
+                        name: playerDetails.name,
+                        createdPlayerId,
+                        country,
+                        mismatchedStats
+                    };
+                }
             }
         }
 
@@ -273,4 +299,5 @@ const getCreatedPlayerDetails = async id => {
     }
 
     fs.writeFileSync(path.resolve(__dirname, '../data/mismatchedPlayerStats.json'), JSON.stringify(mismatchedPlayerStats, null, ' '));
+    fs.writeFileSync(path.resolve(__dirname, '../data/mismatchedPlayerStatsAssociates.json'), JSON.stringify(mismatchedPlayerStatsAssociates, null, ' '));
 })();
